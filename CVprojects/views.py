f377e33b471b4012ee6a989d3edd89c54392dbb1
Siavash Mehran <sia.dev.acc@gmail.com>
2021-04-06 13:32:07 +0430

    paginator added to project_list_view

--- CVprojects/views.py
+++ CVprojects/views.py
@@ -1,17 +1,29 @@
 from CVprojects.models import Categories, Project
 from django.shortcuts import render
 from django.http import Http404
+from django.core.paginator import Paginator
 # Create your views here.
 
-def projects(request):
+
+
+def projects(request, *args, **kwargs):
     
     projectList = Project.objects.get_all_active()
     categories  = Categories.objects.all()
     mostViewd   = Project.objects.get_most_viewd()[:3]
+
+
+    #paginator
+    paginator = Paginator(projectList, 1, allow_empty_first_page=True)
+    pageRequest = request.GET.get('page')
+    pageObj = paginator.get_page(pageRequest)
+
     context = {
-        'list'  :  projectList,
+        'list'       :  pageObj,
         'categories' :  categories,
-        'most_view' : mostViewd
+        'most_view'  : mostViewd,
+        'paginator'  : paginator,
+        'pageRequest': pageRequest
     }
     return render(request, 'projects.html', context)
 
