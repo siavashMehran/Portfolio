9f363db7ac84d31daa465883f67b89a49f8aecfa
Siavash Mehran <sia.dev.acc@gmail.com>
2021-04-24 18:33:16 +0430

    index and about pages cleanup
    index.html is now more simple
    additional info were moved to about me page
    
    views were changed accordingly

--- CVabout/views.py
+++ CVabout/views.py
@@ -1,13 +1,18 @@
-from CVabout.models import Skills
+from django.core.checks.messages import Debug
+from CVabout.models import AboutMe, ContactInfo, Skills
 from django.http.request import HttpRequest
 from django.shortcuts import render
 from random import randint
 # Create your views here.
 
 
-def about(request:HttpRequest):
+def about(request:HttpRequest, *args, **kwargs):
     skills = sorted(Skills.objects.all(), key=lambda x : x.percent + randint(-10, 10), reverse=True)
+    about  = AboutMe.objects.earliest('pk')
+    info   = ContactInfo.objects.earliest('pk')
     context = {
-        'skills' : skills
+        'skills' : skills,
+        'about'  : about,
+        'info'   : info
     }
     return render(request, 'about.html', context)
